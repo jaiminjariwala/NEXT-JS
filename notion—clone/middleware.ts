c97aea219b5fec6d4dc8dbab2d3d4c89cb3c22b1
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-01-20 22:36:10 +0530

    added middleware step mentioned in docs of clerk

---
+++ notion—clone/middleware.ts
@@ -0,0 +1,15 @@
+// this is nextjs middleware component
+// every requests will now go through this clerk—middleware
+
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
