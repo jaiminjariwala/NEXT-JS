c54aaf0b65fea8427c97f6d341820fd807440a65
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-01-25 23:59:44 +0530

    deleted outer firebase files, moved to firebase folder

--- notion—clone/src/components/NewDocumentButton.tsx
+++ notion—clone/src/components/NewDocumentButton.tsx
@@ -1,5 +1,23 @@
+'use client';
+
+import { useTransition } from "react";
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+import { createNewDocument } from "../../actions/actions";
+
 export default function NewDocumentButton() {
+    const [isPending, startTransition] = useTransition()
+    const router = useRouter()
+
+    const handleCreateNewDocument = () => {
+        startTransition(async () => {
+            const result = await createNewDocument()
+            router.push(`/doc/${result.docId}`)
+        })
+    }
     return (
-        <div>NewDocumentButton</div>
+        <Button onClick={handleCreateNewDocument} disabled={!isPending}>
+            {isPending ? "Creating..." : "New Document"}
+        </Button>
     )
 }
