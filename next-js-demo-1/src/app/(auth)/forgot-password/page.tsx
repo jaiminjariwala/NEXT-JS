e9eb23d0c2ab817e5fb516d973dbaf4e94495ffa
jaiminjariwala <157014747+jaiminjariwala@users.noreply.github.com>
2025-10-26 18:18:15 -0400

    Delete next-js-demo-1 directory

--- next-js-demo-1/src/app/(auth)/forgot-password/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function ForgotPassword() {
-    return <h1>ForgotPassword</h1>
-}
-
-// auth❌ but (auth)✅ folder! 
-// This informs Next.js that auth folder should be treated as "Route-Group" and its name should be omitted from the URL path!
--- next-js-demo-1/src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-// we have created our very first route in next.js
-
-// this is the ROOT ROUTE!
-export default function Home() {
-    return <h1>Home Page</h1>
-}
