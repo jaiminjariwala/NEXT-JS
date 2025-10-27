b1512c887eab08613c499e3be3ecda48902a8bcb
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-10-27 15:47:28 -0400

    handling metadata in client components and dynamic metadata

--- routing-demo/src/app/about/page.tsx
+++ routing-demo/src/app/about/page.tsx
@@ -1,3 +1,8 @@
+// the pages metadata takes precedence over the layout metadata if both are defined, that is if both overlap.
+export const metadata = {
+  title: "About Jaimin"
+};
+
 export default function About() {
-    return <h1>About me</h1>
+  return <h1>About Me</h1>;
 }
--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,14 +1,30 @@
+import { Metadata } from "next";
+
+// generateMetadata is an asynchronous function that will return a promise which resolves to a metadata object.
+export const generateMetadata = async ({ params} : Props): Promise<Metadata> => { // this promise resolves to a Metadata object that is... it returns Metadata
+  const id = (await params).productId;
+  const title = await new Promise( (resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${id}`)
+    }, 2000)
+  })
+  return {
+    title: `Product ${title}`
+  } 
+}
+
+type Props = {
+  params : Promise<{ productId: string}>  // this line of code expects params to be a Promise that resolves to an object with a productId string
+}
+
+
 // every page in the "app" router receives route parameters as a prop!
 
 // the "params" object contains route parameters as "key:value" pairs
 // we need to specify the "type" of the "params" object!
 // "params" is an object with a key called "productId" of type "string"
 
-export default async function ProductDetails({
-  params,
-}: {
-  params: Promise<{ productId: string }>;   // this line of code expects params to be a Promise that resolves to an object with a productId string
-}) {
+export default async function ProductDetails({ params } : Props) {
   const productId = (await params).productId;
   return <h1>Details about product {productId}</h1>;
 }
