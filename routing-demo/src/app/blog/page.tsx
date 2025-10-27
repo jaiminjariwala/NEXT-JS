57842212eba37de23527f041b1b0da66608fda8a
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-10-27 16:18:22 -0400

    routing and SEO metadata - default, template and absolute properties of title

--- routing-demo/src/app/blog/page.tsx
+++ routing-demo/src/app/blog/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+//   title: "Hello, this is my BLOG", // My Blog replaces %s in the root layout template.
+  title : {
+    absolute: "Hello, this is my BLOG", // absolute property completely overrides any parent layout title or template.
+  }
+};
 export default function Blog() {
-    return <h1>My Blog</h1>
+  return <h1>My Blog</h1>;
 }
