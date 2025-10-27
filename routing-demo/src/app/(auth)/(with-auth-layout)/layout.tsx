f376a10bf8cb7ae457fe695e43bfa904f2b24ca6
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-10-27 19:38:24 -0400

    implemented active links in nextjs

--- routing-demo/src/app/(auth)/(with-auth-layout)/layout.tsx
+++ routing-demo/src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -1,3 +1,15 @@
+"use client";
+
+import "./styles.css"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navLinks = [
+  { name: "Register", href: "/register" },
+  { name: "Login", href: "/login" },
+  { name: "Forgot Password", href: "/forgot-password" },
+];
+
 // every layout component needs a "children" prop, this is where your page content will go!
 // In this case, the "children" will be either "login/page.tsx" or "register/page.tsx"
 
@@ -6,12 +18,25 @@ export default function AuthLayout({
 }: {
   children: React.ReactNode;
 }) {
-    return (
-        <div>
-            <h2>Inner layout</h2>
-            {children}
-        </div>
-    )
+  const pathname = usePathname();
+  return (
+    <div>
+      {navLinks.map((link) => {
+        const isActive =
+          pathname === link.href ||
+          (pathname.startsWith(link.href) && link.href !== "/");
+        return (
+          <Link
+            className = {isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}
+            href={link.href} key={link.name}>
+            {link.name}
+          </Link>
+        );
+      })}
+      <h2>Inner layout</h2>
+      {children}
+    </div>
+  );
 }
 
 // the routes outside of the group for instance, "forgot-password" folder, do not share the layout, only the "login" and "register" within the ("with-auth-layout" -> route group, which basically ignores the name while routing to the specific URL in website) shares the layout!
