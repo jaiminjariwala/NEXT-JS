a16626ebedf4f980659664f894bf05ada18b26d3
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-11-08 21:48:07 -0500

    designed hero section UI

--- gsap-3js-keyboard-project/src/slices/Hero/index.tsx
+++ gsap-3js-keyboard-project/src/slices/Hero/index.tsx
@@ -15,14 +15,21 @@ const Hero: FC<HeroProps> = ({ slice }) => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="text-white"
+      // for mobile-friendliness make sure to use "dvh" rather than "vh" (viewport height)
+      className="relative h-dvh text-white text-shadow-black/30 text-shadow-lg"
     >
-      <PrismicRichText field={slice.primary.heading} />
-      <PrismicRichText field={slice.primary.body} />
-      <button className="font-bold-slanted group flex w-fit cursor-pointer items-center gap-1 rounded bg-[#01A7E1] px-3 py-1 text-2xl uppercase transition disabled:grayscale">
-        {slice.primary.buy_button_text}
-        <span className="group-hover:translate-x-1">{">"}</span>
-      </button>
+      <div className="hero-scene pointer-events-none sticky top-0 h-dvh w-full">
+        {/* Canvas goes here */}
+      </div>
+
+      <div className="hero-content absolute inset-x-0 top-0 h-dvh">
+        <PrismicRichText field={slice.primary.heading} />
+        <PrismicRichText field={slice.primary.body} />
+        <button className="font-bold-slanted group flex w-fit cursor-pointer items-center gap-1 rounded bg-[#01A7E1] px-3 py-1 text-2xl uppercase transition disabled:grayscale">
+          {slice.primary.buy_button_text}
+          <span className="transition group-hover:translate-x-1">{">"}</span>
+        </button>
+      </div>
     </section>
   );
 };
