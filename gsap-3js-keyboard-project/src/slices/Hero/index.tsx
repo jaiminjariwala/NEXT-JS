270ce2312e08394913060f7efa5a6b8e24c19521
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-11-12 23:37:18 -0500

    create Hero UI and added gltf to jsx code to render keyboard

--- gsap-3js-keyboard-project/src/slices/Hero/index.tsx
+++ gsap-3js-keyboard-project/src/slices/Hero/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { Bounded } from "@/components/Bounded";
 
 /**
  * Props for `Hero`.
@@ -16,19 +17,52 @@ const Hero: FC<HeroProps> = ({ slice }) => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       // for mobile-friendliness make sure to use "dvh" rather than "vh" (viewport height)
-      className="relative h-dvh text-white text-shadow-black/30 text-shadow-lg"
+      className="blue-gradient-bg relative h-dvh text-white text-shadow-black/30 text-shadow-lg"
     >
       <div className="hero-scene pointer-events-none sticky top-0 h-dvh w-full">
         {/* Canvas goes here */}
       </div>
 
       <div className="hero-content absolute inset-x-0 top-0 h-dvh">
-        <PrismicRichText field={slice.primary.heading} />
-        <PrismicRichText field={slice.primary.body} />
-        <button className="font-bold-slanted group flex w-fit cursor-pointer items-center gap-1 rounded bg-[#01A7E1] px-3 py-1 text-2xl uppercase transition disabled:grayscale">
-          {slice.primary.buy_button_text}
-          <span className="transition group-hover:translate-x-1">{">"}</span>
-        </button>
+        <Bounded
+          fullWidth
+          className="absolute inset-x-0 top-18 md:top-24 md:left-[8vw]"
+        >
+          <PrismicRichText
+            field={slice.primary.heading}
+            components={{
+              heading1: ({ children }) => (
+                <h1 className="hero-heading font-black-slanted text-6xl leading-[0.8] uppercase sm:text-7xl lg:text-8xl">
+                  {children}
+                </h1>
+              ),
+            }}
+          />
+        </Bounded>
+
+        <Bounded
+          fullWidth
+          className="hero-body absolute inset-x-0 bottom-0 md:right-[8vw] md:left-auto"
+          innerClassName="flex flex-col gap-3"
+        >
+          <div className="max-w-md">
+            <PrismicRichText
+              field={slice.primary.body}
+              components={{
+                heading2: ({ children }) => (
+                  <h2 className="font-bold-slanted mb-1 text-4xl lg:mb-2 lg:text-6xl">
+                    {children}
+                  </h2>
+                ),
+              }}
+            />
+          </div>
+          <button className="font-bold-slanted group flex w-fit cursor-pointer items-center gap-1 rounded bg-[#01A7E1] px-3 py-1 text-2xl uppercase transition disabled:grayscale">
+            {slice.primary.buy_button_text}
+            <span className="transition group-hover:translate-x-1">{">"}</span>
+            {/* group-hover is going to look for a parent group*/}
+          </button>
+        </Bounded>
       </div>
     </section>
   );
