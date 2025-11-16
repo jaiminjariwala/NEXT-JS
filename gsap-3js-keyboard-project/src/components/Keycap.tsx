f762a7c7528afe8e8a30da1b004e03a69998ac38
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-11-15 23:42:51 -0500

    add useTexture hook to load texture, colorSpace to enhance color visibility

--- gsap-3js-keyboard-project/src/components/Keycap.tsx
+++ gsap-3js-keyboard-project/src/components/Keycap.tsx
@@ -1,10 +1,11 @@
 import * as THREE from "three";
-import { Float, useGLTF } from "@react-three/drei";
+import { Float, useGLTF, useTexture } from "@react-three/drei";
 import { GLTF } from "three-stdlib";
 
 type KeycapProps = {
   position?: [number, number, number];
   rotation?: [number, number, number];
+  texture?: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 };
 type GLTFResult = GLTF & {
   nodes: {
@@ -16,16 +17,35 @@ type GLTFResult = GLTF & {
 export function Keycap({
   position = [0, 0, 0],
   rotation = [0, 0, 0],
+  texture = 0
 }: KeycapProps) {
   const { nodes } = useGLTF("/keycap.gltf") as unknown as GLTFResult;
 
+  const textures =[
+    "/keycap_uv-1.png",
+    "/keycap_uv-2.png",
+    "/keycap_uv-3.png",
+    "/keycap_uv-4.png",
+    "/keycap_uv-5.png",
+    "/keycap_uv-6.png",
+    "/keycap_uv-7.png",
+    "/keycap_uv-8.png",
+    "/keycap_uv-9.png",
+  ]
+
+  const uvTexture = textures[texture]
+  const keycapTexture = useTexture(uvTexture, (texture) => {
+    texture.flipY = false
+    texture.colorSpace = THREE.SRGBColorSpace
+  }) // useTexture hook enables loading our texture for us.
+
   const placeholderMat = new THREE.MeshStandardMaterial({
-    color: "#cccccc",
-    roughness: 0.2,
+    map: keycapTexture,  
+    roughness: 0.2, // value between 0 and 1. 1 will be very rough, it's not gonna be shiny, 0 means completely shiny (perfect reflection)
   });
 
   return (
-    <Float>
+    <Float rotationIntensity={3}>
       <group dispose={null} position={position} rotation={rotation}>
         <mesh
           castShadow
