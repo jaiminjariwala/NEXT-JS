77ce60806f732a0b62ce6637c8ea6cfdda13b6e1
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-11-22 15:01:23 -0500

    added a gsap bouncy keyboard animation on keyboard texture button change

--- gsap-3js-keyboard-project/src/slices/ColorChanger/Scene.tsx
+++ gsap-3js-keyboard-project/src/slices/ColorChanger/Scene.tsx
@@ -1,8 +1,12 @@
 import { Keyboard } from "@/components/Keyboard";
 import { Stage, useTexture } from "@react-three/drei";
 import { KEYCAP_TEXTURES } from ".";
-import { useMemo } from "react";
+import { useMemo, useRef, useState } from "react";
 import * as THREE from "three";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
 
 type SceneProps = {
   selectedTextureId: string;
@@ -10,8 +14,37 @@ type SceneProps = {
 };
 
 export function Scene({ selectedTextureId, onAnimationComplete }: SceneProps) {
+  const keyboardRef = useRef<THREE.Group>(null);
   const texturePaths = KEYCAP_TEXTURES.map((t) => t.path);
   const textures = useTexture(texturePaths);
+  const [currentTextureId, setCurrentTextureId] = useState(selectedTextureId);
+
+  useGSAP(() => {
+    if (!keyboardRef.current || selectedTextureId === currentTextureId) return;
+
+    const keyboard = keyboardRef.current;
+
+    // create timeline
+    const tl = gsap.timeline({
+      onComplete: () => {
+        onAnimationComplete()
+      }
+    });
+
+    tl.to(keyboard.position, {
+      y: 0.3,
+      duration: 0.5,
+      ease: "power2.out",
+      onComplete: () => {
+        setCurrentTextureId(selectedTextureId);
+      },
+    });
+    tl.to(keyboard.position, {
+      y: 0,
+      duration: 0.6,
+      ease: "elastic.out(1, 0.4)",
+    });
+  }, [selectedTextureId, currentTextureId]);
 
   const materials = useMemo(() => {
     const materialMap: { [key: string]: THREE.MeshStandardMaterial } = {};
@@ -42,8 +75,11 @@ export function Scene({ selectedTextureId, onAnimationComplete }: SceneProps) {
 
   return (
     <Stage environment={"city"} intensity={0.01} shadows="contact">
-      <group>
-        <Keyboard keycapMaterial={materials[selectedTextureId]} knobColor={currentKnobColor}/>
+      <group ref={keyboardRef}>
+        <Keyboard
+          keycapMaterial={materials[currentTextureId]}
+          knobColor={currentKnobColor}
+        />
       </group>
     </Stage>
   );
