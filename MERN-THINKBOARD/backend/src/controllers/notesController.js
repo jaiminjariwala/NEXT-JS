8e4b951a06cc9425edc70a694423ef7fe1c8e354
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-11-29 23:21:23 -0500

    created get and post notes method

--- MERN-THINKBOARD/backend/src/controllers/notesController.js
+++ MERN-THINKBOARD/backend/src/controllers/notesController.js
@@ -1,15 +1,42 @@
-export const getAllNotes = (req, res) => {
-  res.status(200).send("You just fetched the notese")
-}
+import Note from "../models/Note.js";
 
-export const createNote = (req, res) => {
-  res.status(201).json({message: "Note created successfully"})
-}
+export const getAllNotes = async (req, res) => {
+  try {
+    const notes = await Note.find(); // Note.find() will give you every single notes
+    res.status(200).json(notes);
+  } catch (error) {
+    console.log("Error in getAllNotes controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
-export const updateNote = (req, res) => {
-  res.status(200).json({message: "Note has been updated"})
-}
+export const createNote = async (req, res) => {
+  try {
+    // to create a note, user is required to enter the title field and content field
+    // we will get the title and content, user entered from req.body
+    const { title, content } = req.body;
 
-export const deleteNote = (req, res) => {
-  res.status(200).json({message: "Note has been deleted"})
-}
+    // create a Note
+    const note = new Note({ title: title, content: content });
+
+    // save it to the database
+    const savedNote = await note.save();
+    res.status(201).json(savedNote);
+  } catch (error) {
+    console.log("Error in creating a note", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const updateNote = async (req, res) => {
+  try {
+    const {title, content} = req.body
+    await Note.findByIdAndUpdate(req.params.id, {title, content}) // first pass the "id" such that we know which note we are updating. Then the fields that we would like to update.
+  } catch (error) {
+
+  }
+};
+
+export const deleteNote = async (req, res) => {
+  res.status(200).json({ message: "Note has been deleted" });
+};
