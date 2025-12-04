c189b2a5409de05980bcc0f9b24596696562f755
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-12-04 13:52:15 -0500

    added updated and delete note controller

--- MERN-THINKBOARD/backend/src/controllers/notesController.js
+++ MERN-THINKBOARD/backend/src/controllers/notesController.js
@@ -30,13 +30,45 @@ export const createNote = async (req, res) => {
 
 export const updateNote = async (req, res) => {
   try {
-    const {title, content} = req.body
-    await Note.findByIdAndUpdate(req.params.id, {title, content}) // first pass the "id" such that we know which note we are updating. Then the fields that we would like to update.
-  } catch (error) {
+    const { title, content } = req.body;
 
+    // check if the id is the valid object id first
+    // 1) "/" in start and "/" in end is the start and end of the regular expression
+    // 2) ^ and $ Anchor: Matches the beginning and endiing of the string. Nothing is allowed before and after the ID.
+    if (!req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
+      return res.status(400).json({ message: "Invalid note id format" });
+    }
+
+    // first pass the "id" such that we know which note we are updating. Then the fields that we would like to update.
+    const updatedNote = await Note.findByIdAndUpdate(
+      req.params.id,
+      { title, content },
+      { new: true, runValidators: true } // this will return the new note with the updated fields.
+    );
+
+    // valid format but the note doesn't exist in the database
+    if (!updatedNote) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+
+    res.status(200).json(updatedNote);
+  } catch (error) {
+    console.log("Error in updateNote controller", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
 export const deleteNote = async (req, res) => {
-  res.status(200).json({ message: "Note has been deleted" });
+  try {
+    const deletedNote = await Note.findByIdAndDelete(req.params.id);
+    if (!deletedNote) {
+      return res
+        .status(404)
+        .json({ message: "Note to be deleted not found :(" });
+    }
+    res.status(200).json({ message: "Note has been deleted successfully!" });
+  } catch (error) {
+    console.log("Error in deleting a note", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
