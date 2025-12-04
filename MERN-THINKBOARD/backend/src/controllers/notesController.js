44ef84c7d4c5c0d52f46a1d86816ca664654435e
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-12-04 14:10:51 -0500

    added getNoteById controller and sorting notes by newest first with .sort({createdAt: -1})

--- MERN-THINKBOARD/backend/src/controllers/notesController.js
+++ MERN-THINKBOARD/backend/src/controllers/notesController.js
@@ -2,11 +2,25 @@ import Note from "../models/Note.js";
 
 export const getAllNotes = async (req, res) => {
   try {
-    const notes = await Note.find(); // Note.find() will give you every single notes
+    // createdAt: -1 will sort in descending order (newest note first)
+    const notes = await Note.find().sort({ createdAt: -1 }); // Note.find() will give you every single notes
     res.status(200).json(notes);
   } catch (error) {
     console.log("Error in getAllNotes controller", error);
     res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getNoteById = async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.id);
+    if (!note) {
+      return res.status(404).json({ message: "Couldn't find the note :(" });
+    }
+    res.json(note);
+  } catch (error) {
+    console.log("Error in getNoteById controller", error);
+    res.status(500).json({ message: "Internal server error!" });
   }
 };
 
--- MERN-THINKBOARD/backend/src/routes/notesRoutes.js
+++ MERN-THINKBOARD/backend/src/routes/notesRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllNotes, createNote, updateNote, deleteNote } from "../controllers/notesController.js";
+import { getAllNotes, createNote, updateNote, deleteNote, getNoteById } from "../controllers/notesController.js";
 
 // create a router
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 // listen for the /api/notes route. Once we get a "get" function, we will run a function which will take the request and response
 // as you hit this url "localhost:5001/api/notes" you are basically requesting for that page, and server responses with res.send("you got 5 notes")
 router.get("/", getAllNotes);
+router.get("/:id", getNoteById)
 router.post("/", createNote); // if user wants to create a node, user will send a post request
 router.put("/:id", updateNote); // to update a note, we will get the "id" to know that which note are we trying to update.
 router.delete("/:id", deleteNote);  // to delete a note, we will again get the id to know that which note are we trying to delete.
