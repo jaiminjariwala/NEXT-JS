6fff8a02aebabc7442920adf69f30ffb2478f8ba
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-12-06 18:29:58 -0500

    feat: once the database connected, only then the server should start at the port

--- MERN-THINKBOARD/backend/src/server.js
+++ MERN-THINKBOARD/backend/src/server.js
@@ -2,27 +2,29 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
-import rateLimiter from "./middleware/rateLimiter.js";   
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 // create express app
 const app = express();
-const PORT = process.env.PORT || 5001;  // we added a fallback value 5001 if process.env.PORT is undefined in any case.
-
-connectDB();
+const PORT = process.env.PORT || 5001; // we added a fallback value 5001 if process.env.PORT is undefined in any case.
 
 // built-in express middleware that parses incoming JSON request bodies so you can easily acess data using req.body
 // it basically reads the incoming request body
-app.use(express.json()) // parse JSON bodies
-app.use(rateLimiter)
+app.use(express.json()); // parse JSON bodies
+app.use(rateLimiter);
 
 // if we get a request that starts with /api/notes then hit "notesRoutes" file
 app.use("/api/notes", notesRoutes);
 
-// listen on port
-app.listen(PORT, () => {
-  console.log("Server started on PORT:", PORT);
+// first connect to DB and then start listening
+// because if the database is not connected(if the connection fails), what is the point of starting the app?
+connectDB().then(() => {
+  // listen on port
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
 });
 
 // What is an endpoint ?
