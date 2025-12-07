58771e50b256d3f1a5cd4eaed8441a6fd22229c4
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-12-06 21:29:30 -0500

    installed react-router and react-hot-toast and created pages

---
+++ MERN-THINKBOARD/frontend/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import {BrowserRouter} from "react-router"
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    {/* we wrapped BrowserRouter around <App /> so, in our entire application, we can use the routing */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
