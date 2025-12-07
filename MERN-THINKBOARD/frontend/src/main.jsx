4062791654ca926aceac379af464bbc8c4bbed95
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-12-07 10:56:27 -0500

    added toaster from react-hot-toast

--- MERN-THINKBOARD/frontend/src/main.jsx
+++ MERN-THINKBOARD/frontend/src/main.jsx
@@ -2,13 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import {BrowserRouter} from "react-router"
+import { BrowserRouter } from "react-router";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* we wrapped BrowserRouter around <App /> so, in our entire application, we can use the routing */}
     <BrowserRouter>
       <App />
+      <Toaster />
     </BrowserRouter>
   </StrictMode>
 );
