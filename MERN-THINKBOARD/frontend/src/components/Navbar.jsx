084d464074f55579299a8e72034c862366c2ab17
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-12-08 22:43:24 -0500

    changed bg color of navbar and new note button

--- MERN-THINKBOARD/frontend/src/components/Navbar.jsx
+++ MERN-THINKBOARD/frontend/src/components/Navbar.jsx
@@ -4,14 +4,14 @@ import { PlusIcon } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <header className="bg-primary/10 border-base-content/7">
+    <header className="bg-[#272343] border-base-content/7">
       <div className="mx-auto max-w-6xl p-4">
         <div className="flex items-center justify-between">
-          <h1 className="text-3xl font-medium text-black tracking-tight">
-            Thinkboard
+          <h1 className="text-3xl font-medium text-white">
+            .notes
           </h1>
           <div className="flex items-center gap-4">
-            <Link to={"/create"} className="btn btn-neutral">
+            <Link to={"/create"} className="flex px-4 items-center gap-2 py-2 bg-white text-black rounded-lg hover:bg-[#f4f4f4]">
               <PlusIcon className="size-5"/>
               <span className="font-medium text-[16px]">New note</span>
             </Link>
