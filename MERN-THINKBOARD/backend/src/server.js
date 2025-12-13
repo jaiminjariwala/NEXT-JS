7faf86805ead62635e5faec88f5e3723f6323841
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-12-12 23:50:29 -0500

    production setup

--- MERN-THINKBOARD/backend/src/server.js
+++ MERN-THINKBOARD/backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -11,20 +12,39 @@ dotenv.config();
 // create express app
 const app = express();
 const PORT = process.env.PORT || 5001; // we added a fallback value 5001 if process.env.PORT is undefined in any case.
+const __dirname = path.resolve(); // saying... we are under the backend
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+// middleware
+if (process.env.NODE_ENV !== 'production') {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 // built-in express middleware that parses incoming JSON request bodies so you can easily acess data using req.body
 // it basically reads the incoming request body
 app.use(express.json()); // parse JSON bodies
 app.use(rateLimiter);
 
-
 // if we get a request that starts with /api/notes then hit "notesRoutes" file
 app.use("/api/notes", notesRoutes);
+
+
+
+
+// if we are in production
+if (process.env.NODE_ENV === "production") {
+  // using a different middleware that comes from express
+  app.use(express.static(path.join(__dirname, "../frontend/dist"))); // this basically says that serve our optimized react application as a static application. Go one step up the backend, go under the frontend and find the "dist" folder and serve them.
+
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  }); // if we get a "get" request other than "/api/notes" mentioned above, we would like to serve our react application
+}
+
+
+
 
 // first connect to DB and then start listening
 // because if the database is not connected(if the connection fails), what is the point of starting the app?
