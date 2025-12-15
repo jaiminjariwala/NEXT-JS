1e21c53ccd0a520cb4afd7c1b23d80d0bd4a6c9f
Jaimin Mukesh Jariwala <jaiminjariwala5@gmail.com>
2025-12-14 19:14:54 -0500

    changed Navbar UI

--- MERN-THINKBOARD/frontend/src/components/Navbar.jsx
+++ MERN-THINKBOARD/frontend/src/components/Navbar.jsx
@@ -4,16 +4,17 @@ import { PlusIcon } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <header className="bg-[#272343] border-base-content/7">
-      <div className="mx-auto max-w-6xl p-4">
+    <header className="bg-[#d8eff9]">
+      <div className="mx-auto max-w-350 p-4">
         <div className="flex items-center justify-between">
-          <h1 className="text-3xl font-medium text-white">
-            .notes
-          </h1>
+          <h1 className="text-3xl font-light text-black hover:text-shadow-base-100-">dot—note</h1>
           <div className="flex items-center gap-4">
-            <Link to={"/create"} className="flex px-4 items-center gap-2 py-2 bg-white text-black rounded-lg hover:bg-[#f4f4f4]">
-              <PlusIcon className="size-5"/>
-              <span className="font-medium text-[16px]">New note</span>
+            <Link
+              to={"/create"}
+              className="flex px-4 items-center gap-1 py-2 bg-white text-black rounded-lg"
+            >
+              <PlusIcon className="size-5 font-medium" />
+              <span className="font-medium hover:font-semibold hover:text-xl text-[18px] transition-all">New note</span>
             </Link>
           </div>
         </div>
